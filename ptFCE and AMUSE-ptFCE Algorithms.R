9c838e1b4fdd3e81a6b65e29a2efa9af9c341034
Kun Meng <60124454+KMengBrown@users.noreply.github.com>
2021-08-17 16:03:22 -0400

    Update ptFCE and AMUSE-ptFCE Algorithms.R

--- ptFCE and AMUSE-ptFCE Algorithms.R
+++ ptFCE and AMUSE-ptFCE Algorithms.R
@@ -23,14 +23,17 @@
 my.fft=function(x, TR.input){ return( fft(x)/length(x) ) }
 
 
-##
+## The following function provides the time-wise demean procedure.
 center_scale <- function(x) { scale(x, scale = FALSE) }
 
 
 ## Project integers to a modulo group.
 ## This projection is prepared for the function periodic extension.
-## e.g., x = [ -7 -6 -5 -4 -3 -2 -1  0  1  2  3  4  5  6  7 ],
-## then modulo.group.function(x) = [3 4 5 1 2 3 4 5 1 2 3 4 5 1 2].
+## e.g., 
+## x=(-7):7
+## for (i in 1:length(x)) { print(modulo.group.function(x[i], 5)) }
+## Then [ -7 -6 -5 -4 -3 -2 -1  0  1  2  3  4  5  6  7 ] becomes
+## [3 4 5 1 2 3 4 5 1 2 3 4 5 1 2].
 modulo.group.function=function(a, n){
   if(a%%n==0){ resp=n }else{ resp=a%%n }
   return(resp)
@@ -52,13 +55,15 @@ Auto_covariance=function(Signal.1, Signal.2){
   
   # Both 'Signal.1' and 'Signal.2' are matrices.
   # Each row of these matrices is a realization of a stochastic process.
-  # Each column corresponds to observations at a specific time point. 
+  # Each column corresponds to observations at a specific time point.
+  # Each row dimension is the number of participants.
+  # Each column dimension is the number fo time points.
   
   resp=vector() 
   for (iter in 0:(dim(Signal.1)[2]-1)) {
     vec.storage=vector()
     for (iter1 in 1:dim(Signal.1)[2]) { 
-      vec.storage[iter1]=mean(Signal.1[,iter1]*Signal.2[, modulo.group.function(iter1+iter, dim(Signal.1)[2])])
+      vec.storage[iter1]=mean(Signal.1[, iter1]*Signal.2[, modulo.group.function(iter1+iter, dim(Signal.1)[2])])
     }
     resp[iter+1]=mean(vec.storage) 
   }
@@ -73,19 +78,22 @@ Auto_covariance=function(Signal.1, Signal.2){
 
 
 ptFCE=function(Y_k, Y_l, 
-             R_k, R_l,
-             TR,
-             cut_off=0.1,
-             freq_plot=TRUE){
+               R_k, R_l,
+               TR,
+               cut_off=0.15,
+               freq_plot=TRUE){
   
   # Y_k and Y_l: They present task-evoked BOLD signals.
   #            * They are matrices.
   #            * Each row is a time series, which is a realization of a stochastic process.
+  #            * The row dimension is the number of participants, and the column dimension
+  #              is the number of time points.
   # R_k and R_l: They present reference signals.
   #            * They are matrices.
   #            * Each row is a time series, which is a realization of a stochastic process.
   # TR:          Repeatition time.
-  # cut_off:     We are interested in the frequencies between 0 and cut_off. Default is 0.1.
+  # cut_off:     We are interested in the frequencies between 0 and cut_off. Default is 0.15
+  #              as HRFs act as band--pass filters (typically 0-0.15 Hz).
   # freq_plot:   If TRUE, the ptFCE() function plots estimates across all Fourier frequencies 
   #              in ( 0, 1/(2*TR) ). Default = TRUE.
   # Output:      An estimation of ptFC corr(beta_k, beta_l).
@@ -98,7 +106,9 @@ ptFCE=function(Y_k, Y_l,
   
   if( (dim(Y_k)[1]!=dim(Y_l)[1]) | (dim(Y_k)[2]!=dim(Y_l)[2])){
     print("The dimensionalities of input matrices are not compatible.")
-  }else{###### The key chunk starts #####
+  }else{
+    
+    ###### The key chunk starts #####
     
     A_k.shifted=matrix(NA, nrow = dim(Y_k)[1], ncol = dim(Y_k)[2])
     A_l.shifted=matrix(NA, nrow = dim(Y_k)[1], ncol = dim(Y_k)[2])
@@ -114,32 +124,30 @@ ptFCE=function(Y_k, Y_l,
       R_l.shifted[iter,]=periodically.shift(R_l[iter,], shift.size = shift)
     }
     
-    # E[Y_k([t-tau])*Y_l([t-tau+s])]
     Auto.cov.kl=Auto_covariance(A_k.shifted, A_l.shifted) 
-    # E[Y_k([t-tau])*Y_k([t-tau+s])]
     Auto.cov.kk=Auto_covariance(A_k.shifted, A_k.shifted) 
-    # E[Y_l([t-tau])*Y_l([t-tau+s])]
     Auto.cov.ll=Auto_covariance(A_l.shifted, A_l.shifted) 
-    # E[R_k([t-tau])*R_l([t-tau+s])]
     Auto.cov.kl.baseline=Auto_covariance(R_k.shifted, R_l.shifted)
-    # E[R_k([t-tau])*R_k([t-tau+s])]
     Auto.cov.kk.baseline=Auto_covariance(R_k.shifted, R_k.shifted)
-    # E[R_l([t-tau])*R_l([t-tau+s])]
     Auto.cov.ll.baseline=Auto_covariance(R_l.shifted, R_l.shifted)
     
-  }###### The key chunk ends #####
+  }
+  ###### The key chunk ends #####
   
   auto_cov=list(A_kl=Auto.cov.kl, A_kk=Auto.cov.kk, A_ll=Auto.cov.ll)
   
   numerator=Mod(my.fft(Auto.cov.kl-Auto.cov.kl.baseline))
   denominator.kk=sqrt(Mod(my.fft(Auto.cov.kk-Auto.cov.kk.baseline)))
   denominator.ll=sqrt(Mod(my.fft(Auto.cov.ll-Auto.cov.ll.baseline)))
-
+  
   vector.all.freq=numerator/(denominator.kk*denominator.ll)
+  vector.all.freq=pmin(vector.all.freq, 1)
+  vector.all.freq=pmax(vector.all.freq, 0)
   frequencies=(0:(dim(Y_k)[2]-1))/(TR*dim(Y_k)[2])
   ind.storage=which(frequencies<cut_off)
+  # The following line restricts the estimation result in [0,1].
   opt.est=max(c(0, min(c(median(vector.all.freq[ind.storage]), 1))))
-  
+
   if(freq_plot==TRUE){
     index.storage=which(frequencies<=1/(2*TR))
     plot(frequencies[index.storage], vector.all.freq[index.storage], 
@@ -156,7 +164,7 @@ ptFCE=function(Y_k, Y_l,
             est_all_freq=vector.all.freq, 
             auto_cov_factors=auto_cov)
   
-  # output: est gives the desired output, i.e., an estimate of ptFC.
+  # output: "est" gives the desired output, i.e., an estimate of ptFC.
   
   return(resp)
   
@@ -185,8 +193,8 @@ library("neuRosim")
 ############################
 
 AMUSE_ptFCE=function(Y_k, Y_l, N, TR,
-                         freq_plot=TRUE, 
-                         cut_off=0.1){
+                     freq_plot=TRUE, 
+                     cut_off=0.1){
   
   # Y_k and Y_l: They present task-evoked BOLD signals.
   #            * They are matrices.
@@ -198,7 +206,7 @@ AMUSE_ptFCE=function(Y_k, Y_l, N, TR,
   # freq_plot and cut_off are explained in ptFCE().
   
   # Observation time points
-  t.variate=(1:(dim(Y_k)[2]))*TR
+  t.variate=((1:(dim(Y_k)[2]))-1)*TR
   
   # Canonical HRF values at observation time points
   cano_HRF=canonicalHRF(t.variate, verbose = FALSE)
@@ -221,6 +229,7 @@ AMUSE_ptFCE=function(Y_k, Y_l, N, TR,
     X_store=matrix(NA, nrow = dim(Y_k)[2], ncol = 2)
     X_store[,1]=Y_k.shifted[iter, ]
     X_store[,2]=N_conv_h.shifted[iter,]-mean(N_conv_h)
+    # The "mean(N_conv_h)" herein is the constant "C_k" in our paper.
     res1<-AMUSE(X_store)
     r_correlation=vector()
     r_correlation[1]=cor(res1$S[,1], N_conv_h.shifted[iter,]-mean(N_conv_h))
